3d979f87a56641dc3d036ed8fa6ae159b94848e2
Pablo Viquez <pablo.viquez@possible.com>
2020-01-27 01:38:25 -0600

    Added dependency on AWS-CLI

--- aws-authenticator.rb
+++ aws-authenticator.rb
@@ -4,6 +4,7 @@ class AwsAuthenticator < Formula
   url "https://github.com/pabloviquez/aws-authenticator/archive/v1.0.3.tar.gz"
   sha256 "ef79f6bdceb00323140cd5bb5938c5102752c5b186ef376ea3b8a750915d5923"
   desc "Interface to authenticate the CLI using AWS with Multi-Factor-Authentication (MFA)"
+  depends_on "awscli"
 
   def install
     bin.install "aws-authenticator"
