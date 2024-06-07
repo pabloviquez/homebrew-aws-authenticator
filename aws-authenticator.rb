d53996c5b0db50feb3b8f2186b92795c47db0ada
Pablo Víquez <pviquez@pabloviquez.com>
2024-06-07 17:10:27 -0600

    Update aws-authenticator.rb

--- aws-authenticator.rb
+++ aws-authenticator.rb
@@ -1,7 +1,7 @@
 class AwsAuthenticator < Formula
   desc "AWS Authenticator interface"
   homepage "https://github.com/pabloviquez/aws-authenticator"
-  url "https://github.com/pabloviquez/aws-authenticator/archive/v1.0.4.tar.gz"
+  url "https://github.com/pabloviquez/aws-authenticator/archive/v1.1.0.tar.gz"
   sha256 "e326a773ee671c741eeca3cb5d4072dbf7fd801900ae268f506b7bd99bf53c61"
   desc "Interface to authenticate the CLI using AWS with Multi-Factor-Authentication (MFA)"
   depends_on "awscli"
